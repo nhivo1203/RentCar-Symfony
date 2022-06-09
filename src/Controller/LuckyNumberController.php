dd6ef650a37141749d4628a3cf8198beee4d5e8a
nhivo1203 <nhivo1604230@gmail.com>
2022-06-09 13:31:33 +0700

    feat: add feature lucky number

---
+++ src/Controller/LuckyNumberController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Controller;
+
+use Exception;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+
+class LuckyNumberController extends AbstractController
+{
+    /**
+     * @Route("/lucky/number")
+     * @throws Exception
+     */
+    public function number(): Response
+    {
+        $number = random_int(0, 100);
+
+        return $this->render('number.html.twig', [
+            'number' => $number,
+        ]);
+    }
+}
