cdccae7ce0974e32b0ed4ea16fc2bceb6b30d1d6
nhivo1203 <nhivo1604230@gmail.com>
2022-06-10 17:12:29 +0700

    feat: add CarController

---
+++ src/Controller/CarController.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Car;
+use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class CarController extends AbstractController
+{
+    /**
+     * @Route("/car", name="add_car")
+     */
+    public function createCar(ManagerRegistry $doctrine): Response
+    {
+        $entityManager = $doctrine->getManager();
+
+        $car = new Car();
+        $car->setName(null);
+        $car->setType('Premium');
+        $car->setBrand('Mercedes');
+        $car->setImage('https://car-rent-nhivo.s3.ap-southeast-1.amazonaws.com/a3fcadc2de72779042369be4a0daef3e6-2-car-png-file.png');
+        $car->setPrice(1000);
+
+        $entityManager->persist($car);
+
+        $entityManager->flush();
+
+        return new Response('Saved Car: ' . $car->getId());
+    }
+
+    /**
+     * @Route("/car/{id}", name="car_show_details")
+     */
+    public function showCarDetails(ManagerRegistry $doctrine, int $id): Response
+    {
+        $car = $doctrine->getRepository(Car::class)->find($id);
+
+        if (!$car) {
+            throw $this->createNotFoundException(
+                'No car found for id ' . $id
+            );
+        }
+
+        return new Response('Check out this great car: ' . $car->getName());
+    }
+
+    /**
+     * @Route("/car/edit/{id}", name="car_edit")
+     */
+    public function updateCar(ManagerRegistry $doctrine, int $id): Response
+    {
+        $entityManager = $doctrine->getManager();
+        $car = $entityManager->getRepository(Car::class)->find($id);
+
+        if (!$car) {
+            throw $this->createNotFoundException(
+                'No car found for id ' . $id
+            );
+        }
+
+        $car->setName('BMW');
+        $entityManager->flush();
+
+        return $this->redirectToRoute('car_show_details', [
+            'id' => $car->getId(),
+        ]);
+    }
+
+    /**
+     * @Route("/car/delete/{id}", name="car_delete")
+     */
+    public function deleteCar(ManagerRegistry $doctrine, int $id): Response
+    {
+        $entityManager = $doctrine->getManager();
+        $car = $entityManager->getRepository(Car::class)->find($id);
+
+        if (!$car) {
+            throw $this->createNotFoundException(
+                'No car found for id ' . $id
+            );
+        }
+
+        $entityManager->remove($car);
+        $entityManager->flush();
+
+        return new Response('Deleted Car: ' . $car->getName());
+    }
+}
