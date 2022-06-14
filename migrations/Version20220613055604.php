4729d159ecc5e4cd52914e2d25da09a3e7cd6256
Nhi Vo <56468937+nhivo1203@users.noreply.github.com>
2022-06-14 09:33:27 +0700

    Update Version20220613055604.php

--- migrations/Version20220613055604.php
+++ migrations/Version20220613055604.php
@@ -23,5 +23,11 @@ final class Version20220613055604 extends AbstractMigration
         $this->addSql('CREATE TABLE user (id INT AUTO_INCREMENT NOT NULL, email VARCHAR(180) NOT NULL, roles JSON NOT NULL, password VARCHAR(255) NOT NULL, UNIQUE INDEX UNIQ_8D93D649E7927C74 (email), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('DROP TABLE car');
     }
+    
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('DROP TABLE user');
+    }
 
 }
